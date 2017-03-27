5436bf7e0a742c28ff78a23aeed48abbb51b416c
David McGrew <mcgrew@cisco.com>
2017-03-27 17:31:59 -0400

    ppi files needed by ppi

---
+++ src/include/ppi.h
@@ -0,0 +1,96 @@
+/*
+ *	
+ * Copyright (c) 2016 Cisco Systems, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *   Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ * 
+ *   Neither the name of the Cisco Systems, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file ppi.h
+ *
+ * \brief PPI module using the generic programming interface defined
+ * in feature.h.
+ *
+ */
+#ifndef PPI_H
+#define PPI_H
+
+#include <stdio.h> 
+#include "output.h"
+#include "feature.h"
+
+#define MAX_NUM_PKT 200
+
+/** usage string */
+#define ppi_usage "  ppi=1                      include per-packet info (ppi)\n"
+
+/** ppi filter key */
+#define ppi_filter(key) 1
+  
+struct pkt_info {
+    struct timeval time; 
+    unsigned int ack;
+    unsigned int seq;
+    unsigned short len;  
+    unsigned char flags;
+};
+
+/** ppi structure */
+typedef struct ppi {
+    unsigned int np;
+    struct pkt_info pkt_info[MAX_NUM_PKT];
+} ppi_t;
+
+declare_feature(ppi);
+
+/** initialization function */
+void ppi_init(struct ppi *ppi);
+
+/** update ppi */
+void ppi_update(struct ppi *ppi, 
+		const struct pcap_pkthdr *header,
+		const void *data, 
+		unsigned int len, 
+		unsigned int report_ppi);
+
+/** JSON print ppi */
+void ppi_print_json(const struct ppi *w1, 
+		     const struct ppi *w2,
+		     zfile f);
+
+/** delete ppi */
+void ppi_delete(struct ppi *ppi);
+
+/** ppi unit test entry point */
+void ppi_unit_test();
+
+#endif /* PPI_H */
