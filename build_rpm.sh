795b03e2a864e399427a74af4e066a0645239710
Brian Long <brilong@cisco.com>
2017-08-30 17:46:02 -0400

    Wrapper script to build joy RPM. Uses git show-ref to determine commit version and short commit version for use in the RPM version.

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -xe
+#
+# Build from master by default.
+# $1 argument can be used to specify another ref (i.e. user-dev).
+#
 
-COMMIT_ID=$(git show-ref --hash refs/heads/brilong-dev)
-SHORT_COMMIT_ID=$(git show-ref --hash=7 refs/heads/brilong-dev)
+GITREF=${1:-master}
+COMMIT_ID=$(git show-ref --hash refs/heads/$GITREF)
+SHORT_COMMIT_ID=$(git show-ref --hash=7 refs/heads/$GITREF)
 TAR_FILE=joy-$SHORT_COMMIT_ID.tar
 TGZ_FILE=joy-$SHORT_COMMIT_ID.tar.gz
 
-#git archive --format=tar --prefix="joy/" $COMMIT_ID | gzip > $TAR_FILE
 git archive --format=tar --prefix="joy/" $COMMIT_ID > $TAR_FILE
 if [ "$?" != 0 ]; then
     echo Failed to create git archive tar
     exit 1
 fi
-tar -uf $TAR_FILE ../joy/Makefile ../joy/install/install-sh ../joy/rpm/joy.spec ../joy/src/anon.c
-if [ "$?" != 0 ]; then
-    echo Failed to update archive tar
-    exit 1
-fi
+#if [ "$GITREF" != "master" ]; then
+#    tar -uf $TAR_FILE ../joy/Makefile ../joy/install/install-sh ../joy/rpm/joy.spec ../joy/src/anon.c
+#    if [ "$?" != 0 ]; then
+#        echo Failed to update archive tar
+#        exit 1
+#    fi
+#fi
 gzip -c $TAR_FILE > $TGZ_FILE
 if [ "$?" != 0 ]; then
     echo Failed to gzip archive tar
@@ -27,5 +33,3 @@ if [ "$?" != 0 ]; then
     exit 1
 fi
 rm -f $TAR_FILE $TGZ_FILE
-
-#cp /usr/src/rpm/RPMS/x86_64/joy-*$SHORT_COMMIT_ID.*.rpm .
